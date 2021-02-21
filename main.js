643718c91038379424fd560ff98b1a56f70011af
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2021-02-21 13:49:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function compile() {
   var html = document.getElementById("html");
-  var css = document.getElementById("css");
-  var js = document.getElementById("js");
+  //var css = document.getElementById("css");
+  //var js = document.getElementById("js");
   var code = document.getElementById("code").contentWindow.document;
 
-  document.body.onkeyup = function() {
+  //document.body.onkeyup = function() {
     code.open();
     code.writeln( html.value );
     code.close();
-  };
+  //};
 }
 /*        "<style>" +
         css.value +
